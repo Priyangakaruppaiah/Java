bf191e756f211e011b8b155f83dbfda60771bd26
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-09-07 06:39:50 +0530

    Create mul2num.java

---
+++ mul2num.java
@@ -0,0 +1,10 @@
+//Multiply two floating-point numbers
+public class MultiplyTwoNumbers {
+  public static void main(String args[]) {
+    float first=1.6f;
+    float second=2.0f;
+    float product=first*second;
+    System.out.println("The product is:"+product);
+  }
+}  
+  
