1c2ec215212eab7670415330041195febcea2142
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-09-09 06:21:58 +0530

    Day-3 completed

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+//Find the given number is odd or even
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner reader=new Scanner(System.in);
+		System.out.println("number");
+		int  num=reader.nextInt();
+		if(num%2==0)
+		{
+		    System.out.println(num+"is even");
+		}
+		else
+		{
+		    System.out.println(num+"is even");
+		}
+	
+	}
+}
