8c5d7e19cf9f5386b67501c1d0702fe303ee6815
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-09-12 06:57:25 +0530

    Day-6 completed

---
+++ 16.Table.java
@@ -0,0 +1,15 @@
+//Java program to generate Multiplication table
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number");
+	    int n1=sc.nextInt();
+	    for(int i=1;i<=10;i++){
+	       int d=n1*i;
+	       System.out.printf("%d * *d = %d \n",n1,i,d);
+	    }
+	
+	}
+}
