cd68f5e610ed7751462a1e85bcc6ae5d75e295f2
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-09-16 20:37:49 +0530

    Day-10 completed

--- 29.ArmstrongLoopFunction.java
+++ 29.ArmstrongLoopFunction.java
@@ -1,4 +1,39 @@
 //Java program to display the armstrong numbers between intervals using function
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number");
+	    int low=sc.nextInt();
+	      System.out.println("Enter the number");
+	    int high=sc.nextInt();
+	    for(int number=low+1;number<high;++number){
+	        if(arm(number)){
+	            System.out.println(number+" ");
+	        }
+	    }
+	   public static boolean arm(int num){
+	       int digit=0;
+	       int result=0;
+	       int originalnumber=num;
+	       while(originalnumber!=0){
+	           originalnumber/=10;
+               ++digits;
+	       }
+	       originalnumber=num;
+	       while(originalnumber!=0){
+	           int remainder=originalnumber%10;
+	           result+=Math.pow(remainder,digits);
+	           originalnumber/=10;
+	       }
+	       if(result==num)
+	          return true;
+	       return false;   
+	    
+		
+	}
+}
 
 
 
