0b72e3fc8aacc7c45559d4d313b938623858603f
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-01 19:52:44 +0530

    Day-24 completed

---
+++ 68.CurrentWorking.java
@@ -0,0 +1,8 @@
+//Get current working directory
+public class Main
+{
+	public static void main(String[] args) {
+	    String path=System.getProperty("stringarray.html");
+		System.out.println("Working Directory = " +path);
+	}
+}
