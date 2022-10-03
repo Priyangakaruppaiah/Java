c1039fb0442840b390aecf64de83e68e530e6f0a
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-04 05:09:45 +0530

    Day-27 completed

---
+++ 76.InputstreamToString.java
@@ -0,0 +1,18 @@
+//Java program to convert  InputStream to String
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args)throws IOException {
+	    InputStream stream=new ByteArrayInputStream("Hello there!" . getBytes());
+	    StringBuilder sb=new StringBuilder();
+	    String line;
+	    
+	    BufferedReader br=new BufferedReader(new  InputStreamReader(stream) );
+	    while((line = br.readLine()) != null){
+	        sb.append(line);
+	    }
+	    br.close();
+		System.out.println(sb);
+	}
+}
+
