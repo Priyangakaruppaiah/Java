a191c1e7ca2809423445a573836580711cc47396
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-05 20:33:44 +0530

    Day-28 completed

---
+++ 80.MapbyValues.java
@@ -0,0 +1,18 @@
+//Java program to sort a Map by values
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter String:");
+	    String st1=sc.nextLine();
+	    String st2=sc.nextLine();
+	    if(st1.equals(st2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+		
+	}
+}
+        
+
