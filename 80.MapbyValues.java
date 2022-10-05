47190ba5d052b23783d69861b259831f5bf91d60
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-05 20:42:58 +0530

    Day-28 completed

--- 80.MapbyValues.java
+++ 80.MapbyValues.java
@@ -1,18 +1,43 @@
 //Java program to sort a Map by values
-import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    System.out.println("Enter String:");
-	    String st1=sc.nextLine();
-	    String st2=sc.nextLine();
-	    if(st1.equals(st2))
-            System.out.println("Equal");
-        else
-            System.out.println("Not Equal");
-		
-	}
+import java.util.*;
+import java.util.Map.Entry;
+
+class Main {
+
+  public static void main(String[] args) {
+    LinkedHashMap<String, String> capitals = new LinkedHashMap();
+    capitals.put("Nepal", "Kathmandu");
+    capitals.put("India", "New Delhi");
+    capitals.put("United States", "Washington");
+    capitals.put("England", "London");
+    capitals.put("Australia", "Canberra");
+
+    
+    Map<String, String> result = sortMap(capitals);
+
+    for (Map.Entry entry : result.entrySet()) {
+      System.out.print("Key: " + entry.getKey());
+      System.out.println(" Value: " + entry.getValue());
+    }
+  }
+
+  public static LinkedHashMap sortMap(LinkedHashMap map) {
+    List <Entry<String, String>> capitalList = new LinkedList<>(map.entrySet());
+
+    
+    Collections.sort(capitalList, (l1, l2) -> l1.getValue().compareTo(l2.getValue()));
+
+    
+    LinkedHashMap<String, String> result = new LinkedHashMap();
+
+    
+    for (Map.Entry<String, String> entry : capitalList) {
+      result.put(entry.getKey(), entry.getValue());
+    }
+
+    return result;
+  }
 }
+
         
 
