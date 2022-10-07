637da709d9f6cd8993034f32db3fe1f0461d576e
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-07 17:32:52 +0530

    Day-30 completed

---
+++ 85.LongToInt.java
@@ -0,0 +1,15 @@
+//Java program to convert long type variables into int
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	     Scanner sc=new Scanner(System.in);
+	    long num1=sc.nextLong();
+	    long num2=sc.nextLong();
+	    int a = (int)num1;
+	    int b = (int)num2;
+		System.out.println(a);
+		System.out.println(b);
+	
+	}
+}
