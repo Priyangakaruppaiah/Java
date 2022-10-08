e2326e8a0a8a2270a2d3cacad97a8ce633af4b53
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-08 11:00:50 +0530

    Day-31 completed

---
+++ 89.StringToInt.java
@@ -0,0 +1,14 @@
+//Java program to convert string to int
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		int b1=Integer.parseInt(s1);
+		int b2=Integer.parseInt(s2);
+		System.out.println(b1);
+		System.out.println(b2);
+	}
+}
