6b50efd6cac208471d051ddfa27b4bbd5e3578d1
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-08 11:07:00 +0530

    Day-31 completed

---
+++ 90.IntToString.java
@@ -0,0 +1,14 @@
+//Java program to convert int to string 
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		String b1=String.valueOf(n1);
+		String b2=String.valueOf(n2);
+		System.out.println(b1);
+		System.out.println(b2);
+	}
+}
