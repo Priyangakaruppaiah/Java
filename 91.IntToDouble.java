987ba37ac6d56a8da1e0f48d4e45de5800ac6065
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-09 09:26:05 +0530

    Day-32 completed

---
+++ 91.IntToDouble.java
@@ -0,0 +1,15 @@
+//Java program to convert int type variables to double
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n1=sc.nextInt();
+	     int n2=sc.nextInt();
+	     double c=n1;
+	     double d=n2;
+	     System.out.println(c);
+	     System.out.println(d);
+	 }
+}
+
