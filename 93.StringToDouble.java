a1e10b150fc96a91847e9f800025eb2bcb587b87
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-09 09:32:13 +0530

    Day-32 completed

---
+++ 93.StringToDouble.java
@@ -0,0 +1,14 @@
+//Java program to convert string variables to double
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String n1=sc.nextLine();
+		String n2=sc.nextLine();
+		double c=Double.parseDouble(n1);
+		double d=Double.parseDouble(n2);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
