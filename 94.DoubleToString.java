2477f376bc7065a3e125a0937ff4d329e8037f61
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-10 06:42:42 +0530

    Day-33 completed

---
+++ 94.DoubleToString.java
@@ -0,0 +1,14 @@
+//Java program to convert double type variables to string
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    double n1=sc.nextDouble();
+	    double n2=sc.nextDouble();
+	    String s1=String.valueOf(n1);
+	    String s2=String.valueOf(n2);
+	    System.out.println(s1);
+	      System.out.println(s2);
+	}
+}
