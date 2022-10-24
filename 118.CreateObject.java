ef16edbaa0832c81151ed68f78d8ab459630470e
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-24 17:46:06 +0530

    Create 118.CreateObject.java

---
+++ 118.CreateObject.java
@@ -0,0 +1,15 @@
+//Java program to print the object
+class Test {
+
+}
+
+class Main {
+  public static void main(String[] args) {
+
+    // create an object of the Test class
+    Test obj = new Test();
+
+    // print the object
+    System.out.println(obj);
+  }
+}
