8c06ae30a1162e59a207c250e0d88c32c8ea6b9b
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-10-28 18:29:31 +0530

    Day-46 completed

---
+++ 131.NameVersion.java
@@ -0,0 +1,8 @@
+//Java Program to Determine the name and version of the operating system
+class Main {
+  public static void main(String[] args) {
+    String operatingSystem = System.getProperty("os.name");
+    System.out.println(operatingSystem);
+
+  }
+}
