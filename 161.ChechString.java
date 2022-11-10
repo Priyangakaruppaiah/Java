f8b2fc3b480b3984e6cf927b173cc636c92ae36c
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-10 06:57:04 +0530

    Day-59 completed

---
+++ 161.ChechString.java
@@ -0,0 +1,25 @@
+//Java Program to Check if a string contains a substring
+class Main {
+  public static void main(String[] args) {
+  
+    String txt = "This is Programiz";
+    String str1 = "Programiz";
+    String str2 = "Programming";
+
+    boolean result = txt.contains(str1);
+    if(result) {
+      System.out.println(str1 + " is present in the string.");
+    }
+    else {
+      System.out.println(str1 + " is not present in the string.");
+    }
+
+    result = txt.contains(str2);
+    if(result) {
+      System.out.println(str2 + " is present in the string.");
+    }
+    else {
+      System.out.println(str2 + " is not present in the string.");
+    }
+  }
+}
