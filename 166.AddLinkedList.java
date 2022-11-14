2c5052d8a7fe9b30685a01637ac270794f782209
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-14 18:50:58 +0530

    Day-63 completed

---
+++ 166.AddLinkedList.java
@@ -0,0 +1,16 @@
+//Java Program to Add elements to a LinkedList
+import java.util.LinkedList;
+
+class Main {
+  public static void main(String[] args){
+
+    // create a linkedlist
+    LinkedList<String> languages = new LinkedList<>();
+
+    // Add elements to LinkedList
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    System.out.println("LinkedList: " + languages);
+  }
+}
