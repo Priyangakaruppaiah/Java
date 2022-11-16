e51358db66a04ea1be37ae192bc1227920671bdd
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-16 07:28:49 +0530

    Day-65 completed

---
+++ 171.StringReverse.java
@@ -0,0 +1,14 @@
+ // String Reverse in java
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="college";
+	    char[] s1 = str.toCharArray();
+  
+        for (int i = s1.length - 1; i >= 0; i--)
+            System.out.print(s1[i]);
+       
+	   	
+	}
+}
+
