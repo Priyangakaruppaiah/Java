51b52f1c5e9c960cc23250651400dce5145416df
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-10 19:38:01 +0530

    Day-71c completed

---
+++ 177.DecimalOctal.java
@@ -0,0 +1,12 @@
+public class Main{  
+public static void main(String args[]){  
+System.out.println(Integer.toOctalString(8));  
+System.out.println(Integer.toOctalString(19));  
+System.out.println(Integer.toOctalString(81));  
+}}  
+/*
+OUTPUT:
+10
+23
+121
+*/
