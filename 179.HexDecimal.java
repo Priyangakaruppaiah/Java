b0ebc088a6befb121f227910fd6433e502e741e9
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-12 17:44:01 +0530

    Day-73 completed

---
+++ 179.HexDecimal.java
@@ -0,0 +1,10 @@
+public class Main{  
+public static void main(String args[]){  
+String hex="a";  
+int decimal=Integer.parseInt(hex,16);  
+System.out.println(decimal);  
+}}  
+/*
+OUTPUT:
+10
+*/
