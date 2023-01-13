01c27980c62b303a35881ad608d01ddc14ddebb9
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-13 19:36:35 +0530

    Day-24 completed

---
+++ 180.DecimalToBinary.java
@@ -0,0 +1,12 @@
+public class Main{  
+public static void main(String args[]){  
+System.out.println(Integer.toBinaryString(10));  
+System.out.println(Integer.toBinaryString(21));  
+System.out.println(Integer.toBinaryString(31));  
+}} 
+/*
+OUTPUT:
+1010
+10101
+11111
+*/
