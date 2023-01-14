da9969d34bcec9b3ad1bc80635c99de7e507961c
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-14 22:06:05 +0530

    Day-75 completed

---
+++ 181.BinaryToDecimal.java
@@ -0,0 +1,10 @@
+public class BinaryToDecimalExample1{  
+public static void main(String args[]){  
+String binaryString="1010";  
+int decimal=Integer.parseInt(binaryString,2);  
+System.out.println(decimal);  
+}}  
+/*
+OUTPUT:
+10
+*/
