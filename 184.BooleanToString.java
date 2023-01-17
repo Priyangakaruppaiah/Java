2ba6ee68f615e72389cc3a34439c079b87062a69
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-17 19:11:57 +0530

    Day-78 completed

---
+++ 184.BooleanToString.java
@@ -0,0 +1,15 @@
+/Convert Boolean to String
+public class Main{  
+public static void main(String args[]){  
+boolean b1=true;  
+boolean b2=false;  
+String s1=String.valueOf(b1);  
+String s2=String.valueOf(b2);  
+System.out.println(s1);  
+System.out.println(s2);  
+}}
+/*
+OUTPUT:
+true
+false
+*/
