d433def3e13d12d7da24ea677d494749f111c280
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-20 18:17:33 +0530

    Day-81 completed

---
+++ 187.CharToInt.java
@@ -0,0 +1,14 @@
+public class Main{  
+public static void main(String args[]){  
+char c='a';  
+char c2='1';  
+int a=c;  
+int b=c2;  
+System.out.println(a);  
+System.out.println(b);  
+}}  
+/*
+OUTPUT:
+97
+49
+*/
