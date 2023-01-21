8ab5b9570c3b74ad40dc47cdb6d365db352acd11
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-21 16:43:03 +0530

    Day-82 completed

---
+++ 188.DoubleToInt.java
@@ -0,0 +1,11 @@
+//Java program to convert Double to  Int
+public class Main{  
+public static void main(String args[]){  
+double d=10.5;  
+int i=(int)d;  
+System.out.println(i);  
+}}  
+/*
+OUTPUT:
+10
+*/
