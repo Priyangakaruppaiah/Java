82f7c240b4f9c70a1bd87d828f96f508e6dc2b94
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-23 19:05:03 +0530

    Day-83 completed

---
+++ 189.LongToInt.java
@@ -0,0 +1,12 @@
+// Java program to convert Long to int
+public class Main{  
+public static void main(String args[]){  
+long l=500;  
+int i=(int)l;  
+System.out.println(i);  
+}} 
+/*
+OUTPUT:
+500
+*/
+
