6165d526b554a486f25a0ac64c861a19e8ca6ad7
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-27 19:23:30 +0530

    Day-85 completed

---
+++ 191.IntToLong.java
@@ -0,0 +1,12 @@
+//Java program to convert int to long
+public class Main{  
+public static void main(String args[]){  
+int i=200;  
+long l=i;  
+System.out.println(l);  
+}}  
+/*
+OUTPUT:
+200
+*/
+
