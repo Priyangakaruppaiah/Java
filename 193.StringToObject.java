cd9d475d97b88867d674193d4131dcda155983eb
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-30 15:15:40 +0530

    Day-86 completed

---
+++ 193.StringToObject.java
@@ -0,0 +1,11 @@
+//Convert String to Object
+public class Main{  
+public static void main(String args[]){  
+String s="hello";  
+Object obj=s;  
+System.out.println(obj);  
+}}  
+/*
+OUTPUT:
+hello
+*/
