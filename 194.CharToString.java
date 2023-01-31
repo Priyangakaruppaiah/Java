923baef095dfa6c02acc32b9849502da09100a5d
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-01-31 14:06:45 +0530

    Day-87 completed

---
+++ 194.CharToString.java
@@ -0,0 +1,11 @@
+//Java program to convert char to string
+public class Main{  
+public static void main(String args[]){  
+char c='S';  
+String s=String.valueOf(c);  
+System.out.println("String is: "+s);  
+}}  
+/*
+OUTPUT:
+String is: S
+*/
