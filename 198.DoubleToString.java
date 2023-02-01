f978ac31e35c41441172a402219b0267cfb3e2d8
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-01 20:29:43 +0530

    Day-88 completed

---
+++ 198.DoubleToString.java
@@ -0,0 +1,11 @@
+//Java program to convert Double to String
+public class Main{  
+public static void main(String args[]){  
+double d=12.3;  
+String s=String.valueOf(d);  
+System.out.println(s);  
+}}  
+/*
+OUTPUT:
+12.3
+*/
