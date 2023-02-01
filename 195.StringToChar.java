2ac6f3eb9071aa4b7661e6a7607f41c78e3e9562
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-01 20:18:43 +0530

    Day-88 completed

---
+++ 195.StringToChar.java
@@ -0,0 +1,11 @@
+// Java program to convert String to Char
+public class Main{  
+public static void main(String args[]){  
+String s="hello";  
+char c=s.charAt(0);//returns h  
+System.out.println("1st character is: "+c);  
+}}
+/*
+OUTPUT:
+1st character is: h
+*/
