a84609da9b922d778b4a65cc5b0a2186a32bba04
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-02 18:55:29 +0530

    Day-89 completed

---
+++ 199.StringToDouble.java
@@ -0,0 +1,11 @@
+//Java program to convert String to Double
+public class Main{  
+public static void main(String args[]){  
+String s="23.6";  
+double d=Double.parseDouble("23.6");  
+System.out.println(d);  
+}}
+/*
+OUTPUT:
+23.6
+*/
