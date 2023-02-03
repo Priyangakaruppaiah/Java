152186af0eaf3e4a2a28284aafa0501ea920c62b
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-03 11:37:13 +0530

    Day-90 completed

---
+++ 200.FloatToString.java
@@ -0,0 +1,11 @@
+// Java program to convert Float to String
+public class Main{  
+public static void main(String args[]){  
+float f=12.3F;//F is the suffix for float  
+String s=String.valueOf(f);  
+System.out.println(s);  
+}}  
+/*
+OUTPUT:
+12.3
+*/
