5ea220bbb9b34b654cfe9ad9bcf01d3eb6f96e20
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-04 18:39:15 +0530

    Day-91 completed

---
+++ 201.StringToFloat.java
@@ -0,0 +1,11 @@
+// Java program to convert String to Float
+public class Main{  
+public static void main(String args[]){  
+String s="23.6";  
+float f=Float.parseFloat("23.6");  
+System.out.println(f);  
+}}
+/*
+OUTPUT:
+23.6
+*/
