3b43bfe9136e0758f3c370babca2e7f3c71946dc
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-06 19:15:08 +0530

    Day-93 completed

---
+++ 203.StringToLong.java
@@ -0,0 +1,11 @@
+// Java program to convert String to Long
+public class Main{  
+public static void main(String args[]){  
+String s="9990449935";  
+long l=Long.parseLong(s);  
+System.out.println(l);  
+}}
+/*
+OUTPUT:
+9990449935
+*/
