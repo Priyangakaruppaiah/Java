016039fd00f412c8ae00562aa0320207aba39fe1
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-08 08:31:27 +0530

    Day-95 completed

---
+++ 205.StringToInt.java
@@ -0,0 +1,11 @@
+// Java program to convert String to Int
+public class Main{  
+public static void main(String args[]){  
+String s="200";  
+int i=Integer.parseInt(s);  
+System.out.println(i);  
+}} 
+/*
+OUTPUT:
+200
+*/
