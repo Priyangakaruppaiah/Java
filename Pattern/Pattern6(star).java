9703631166db109a58687d92a8540ad8c7b9473e
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-21 07:00:30 +0530

    Day-102 completed

---
+++ Pattern/Pattern6(star).java
@@ -0,0 +1,26 @@
+class Main
+{
+  public static void main(String[] args)  
+   {  
+   int i,j, n=7;  
+   System.out.println("Right angle triangle");  
+   for(i=1;i<n;i++)  
+   {  
+   for(j=1;j<=i;j++)  
+   {  
+       System.out.print("*");  
+   }  
+       System.out.println("");  
+   }  
+   }  
+} 
+/*
+OUTPUT:
+Right angle triangle
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ * * * * * *
+*/ 
