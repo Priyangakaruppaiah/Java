a596ecb875660bf58cee2f640d35bf38e474717c
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-26 10:36:49 +0530

    Day-106 completed

--- Pattern/Pattern8.java
+++ Pattern/Pattern8.java
@@ -1,1 +1,23 @@
-Java
+public class Main 
+{  
+    public static void main(String[] args)  
+    {  
+        int n = 5;  
+        for(int i = 0 ; i <= n ; i++)  
+        {  
+        for(int j = 0 ; j < i ; j++)  
+        {  
+            System.out.print(j+1);  
+        }  
+            System.out.println("");  
+        }  
+    }  
+} 
+/*
+OUTPUT:
+1
+12
+123
+1234
+12345
+*/
