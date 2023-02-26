6c18a17b785785acda54324157d5a538b81f0dd3
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-02-26 10:49:03 +0530

    Day-107 completed

---
+++ Pattern/Pattern9.java
@@ -0,0 +1,26 @@
+class Main
+{     
+   public static void main(String[] args)     
+   {    
+        int i,j;    
+        int n=6;    
+        for(i=n;i>0;i--)    
+        {    
+            for(j=0;j<i;j++)    
+            {    
+                System.out.print("*");    
+            }    
+            System.out.println("");    
+        }        
+   }
+}
+/*
+OUTPUT:
+******
+*****
+****
+***
+**
+*
+*/
+    
