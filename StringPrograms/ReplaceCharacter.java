08564953a2cb254d3497ca9803ded242559ce883
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-03-01 07:18:14 +0530

    Day-109 completed

--- StringPrograms/ReplaceCharacter.java
+++ StringPrograms/ReplaceCharacter.java
@@ -1,1 +1,14 @@
-
+//Java Program to replace the spaces of a string with a specific character
+public class Main    
+{    
+    public static void main(String[] args) {    
+        String string = "Once in a blue moon";    
+        char ch = '-';    
+        string = string.replace(' ', ch);    
+        System.out.println(string);    
+    }    
+}
+/*
+OUTPUT:
+Once-in-a-blue-moon
+*/
