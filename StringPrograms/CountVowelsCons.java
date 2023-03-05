a3b412d761628da4d23679ed4d6b39a89d352101
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-03-05 09:50:19 +0530

    Day-112 completed

---
+++ StringPrograms/CountVowelsCons.java
@@ -0,0 +1,23 @@
+//Java Program to count the total number of vowels and consonants in a string
+public class Main {    
+    public static void main(String[] args) {    
+        int vCount=0,cCount=0;    
+        String str = "This is a really simple sentence";    
+        str = str.toLowerCase();    
+        for(int i=0;i<str.length();i++) {    
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u'){    
+                vCount++;    
+            }    
+            else if(str.charAt(i)>='a' && str.charAt(i)<='z') {      
+                 cCount++;    
+            }    
+        }    
+        System.out.println("Number of vowels: " + vCount);    
+        System.out.println("Number of consonants: " + cCount);    
+    }    
+} 
+/*
+OUTPUT:
+Number of vowels: 10
+Number of consonants: 17
+*/
