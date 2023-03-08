c0a6e452d4dbaaf6e4e1308357f8f1d7bc9fef04
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-03-08 06:41:29 +0530

    Day-118 completed

---
+++ StringPrograms/Anagram.java
@@ -0,0 +1,29 @@
+//Java Program to determine whether two strings are the anagram
+import java.util.Arrays;  
+public class Main{  
+    public static void main (String [] args) {  
+        String str1="Brag";  
+        String str2="Grab";  
+        str1 = str1.toLowerCase();  
+        str2 = str2.toLowerCase();  
+        if(str1.length()!= str2.length()) {  
+            System.out.println("Both the strings are not anagram");  
+        }  
+        else {  
+            char[] string1 = str1.toCharArray();  
+            char[] string2 = str2.toCharArray();  
+            Arrays.sort(string1);  
+            Arrays.sort(string2);  
+            if(Arrays.equals(string1, string2) == true) {  
+                System.out.println("Both the strings are anagram");  
+            }  
+            else {  
+                System.out.println("Both the strings are not anagram");  
+            }  
+        }  
+    }  
+} 
+/*
+OUTPUT:
+Both the strings are anagram.
+*/
