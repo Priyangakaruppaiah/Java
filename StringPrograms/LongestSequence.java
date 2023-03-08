4f9c6f41eeabfa76777e88734ac122063d4b1d64
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-03-08 06:50:36 +0530

    Day-120 completed

---
+++ StringPrograms/LongestSequence.java
@@ -0,0 +1,29 @@
+//Java Program to find the longest repeating sequence in a string
+public class Main{  
+    public static String lcp(String s, String t){  
+        int n =Math.min(s.length(),t.length());  
+        for(int i=0;i<n;i++){  
+            if(s.charAt(i)!=t.charAt(i)){  
+                return s.substring(0,i);  
+            }  
+        }  
+        return s.substring(0,n);  
+    }  
+    public static void main(String[] args) {  
+        String str="acbdfghybdf";  
+        String lrs="";  
+        int n=str.length();  
+        for(int i=0;i<n;i++){  
+            for(int j=i+1;j<n;j++){  
+                String x=lcp(str.substring(i,n),str.substring(j,n));  
+                if(x.length()>lrs.length()) 
+                    lrs=x;  
+            }  
+        }  
+        System.out.println("Longest repeating sequence: "+lrs);  
+    }  
+}  
+/*
+OUTPUT:
+Longest repeating sequence: bdf
+*/
