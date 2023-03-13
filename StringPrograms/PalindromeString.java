058bc35bca97e2c071a4beb266dd1e1dc043db76
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-03-13 15:12:42 +0530

    Day-124 completed

---
+++ StringPrograms/PalindromeString.java
@@ -0,0 +1,23 @@
+//Java Program to determine whether a given string is palindrome
+public class Main   
+{    
+    public static void main(String[] args) {    
+        String string = "Kayak";    
+        boolean flag = true;    
+        string = string.toLowerCase();    
+        for(int i = 0; i < string.length()/2; i++){    
+            if(string.charAt(i) != string.charAt(string.length()-i-1)){    
+                flag = false;    
+                break;    
+            }    
+        }    
+        if(flag)    
+            System.out.println("Given string is palindrome");    
+        else    
+            System.out.println("Given string is not a palindrome");    
+    }    
+}  
+/*
+OUTPUT:
+Given string is palindrome
+*/
