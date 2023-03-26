81861510b9f3ed03f53b12081b6eb9b69a55b279
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-03-26 08:05:27 +0530

    Day-136 completed

---
+++ StringPrograms/StrRevFun.java
@@ -0,0 +1,27 @@
+//How to Reverse a String in Java Word by Word
+import java.util.Scanner;  
+public class Main  
+{  
+public static void main(String[] args)   
+{  
+String str;  
+System.out.println("Enter a string: ");  
+Scanner scanner = new Scanner(System.in);  
+str = scanner.nextLine();  
+scanner.close();                                //closes the input stream  
+String reversed = reverseString(str);  
+System.out.println("The reversed string is: " + reversed);  
+}  
+public static String reverseString(String s)  
+{  
+if (s.isEmpty())                            //checks the string if empty  
+return s;  
+return reverseString(s.substring(1)) + s.charAt(0);                     //recursively called function  
+}  
+}  
+/*
+OUTPUT:
+Enter a string: 
+beautiful
+The reversed string is: lufituaeb
+*/
