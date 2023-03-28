db972b6988f8c304a16f3fa73f3b5112e21434f1
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-03-28 06:53:56 +0530

    Day-137 completed

---
+++ StringPrograms/SmallBig.java
@@ -0,0 +1,18 @@
+//Program to print smallest and biggest possible palindrome word in a given string
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+public class Main 
+{  
+    public static void main(String[] args) throws Exception {  
+        String line;  
+        int count = 0;  
+        FileReader file = new FileReader("data.txt ");  
+        BufferedReader br = new BufferedReader(file);  
+        while((line = br.readLine()) != null) {  
+            String words[] = line.split("");  
+            count = count + words.length;  
+        }  
+        System.out.println("Number of words present in given file: " + count);  
+        br.close();  
+    }  
+}  
