a87e99d1b8f65494632faffc4bd7c7bee9d6e01d
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-04-01 18:06:14 +0530

    Day-141 completed

---
+++ 207.Array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("entae the array");
+		int n1=sc.nextInt();
+		int a[]=new int[n1];
+			System.out.println("Enter the elements:");
+			for(int i=0;i<n1;i++){
+			    a[i]=sc.nextInt();
+			}
+	  for(int i=0;i<n1;i++){
+	    System.out.println(a[i]);
+	  }
+  }
+}
