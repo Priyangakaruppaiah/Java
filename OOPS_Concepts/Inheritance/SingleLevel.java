87760bdf73d94251938081fcd6304cade5a30721
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-04-27 08:00:42 +0530

    Day-161 completed

---
+++ OOPS_Concepts/Inheritance/SingleLevel.java
@@ -0,0 +1,25 @@
+class Animal{  
+    void eat(){
+        System.out.println("eating...");
+         
+     }  
+}  
+class Dog extends Animal{  
+    void bark(){
+        System.out.println("barking...");
+        
+    }  
+}  
+class Main{  
+    public static void main(String args[]){  
+        Dog d=new Dog();  
+        d.bark();  
+        d.eat();  
+    }
+    
+}
+/*
+OUTPUT:
+barking...
+eating...
+*/
