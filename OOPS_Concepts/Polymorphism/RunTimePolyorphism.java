855777d7f6b9dc93f239f56a2981a1d3ac7dde38
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-05-03 16:56:34 +0530

    Day-167 completed

---
+++ OOPS_Concepts/Polymorphism/RunTimePolyorphism.java
@@ -0,0 +1,51 @@
+class Shape
+ {  
+  void draw()
+   {
+       System.out.println("drawing...");
+       
+   }  
+ }  
+ class Rectangle extends Shape
+ {  
+  void draw()
+   {
+       System.out.println("drawing rectangle...");
+       
+   }  
+}  
+ class Circle extends Shape
+  {  
+   void draw()
+    {
+        System.out.println("drawing circle...");
+        
+    }  
+}  
+  class Triangle extends Shape
+  {  
+    void draw()
+    {
+        System.out.println("drawing triangle...");
+        
+    }  
+}  
+class Main
+{  
+   public static void main(String args[])
+   {  
+     Shape s;  
+     s=new Rectangle();  
+     s.draw();  
+     s=new Circle();  
+     s.draw();  
+     s=new Triangle();  
+     s.draw();  
+    }  
+ } 
+/*
+OUTPUT:
+drawing rectangle...
+drawing circle...
+drawing triangle...
+*/
