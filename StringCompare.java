4b73691c718d7d08ad25688abb2cd1de9790d305
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-05-12 07:18:02 +0530

    Day-172 completed

---
+++ StringCompare.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    String first = "java programming";
+    String second = "java programming";
+    String third = "python programming";
+
+    boolean result1 = first.equals(second);
+    System.out.println("Strings first and second are equal: " + result1);
+
+    boolean result2 = first.equals(third);
+    System.out.println("Strings first and third are equal: " + result2);
+  }
+}
+/*
+OUTPUT:
+Strings first and second are equal: true
+Strings first and third are equal: false
+*/
