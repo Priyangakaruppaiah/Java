ad458f2566a92826a9dccba81de96bb57dd58da7
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2023-10-10 10:50:00 +0530

    Day-196 completed

---
+++ CharCount.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String s = sc.nextLine();
+        int count =0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)==s.charAt(0))
+            {
+                count++;
+            }
+        }
+        if(count>0)
+        {
+            System.out.println(count);
+        }
+        else{
+            System.out.println("NA");
+        }
+    }
+}
+/*
+INPUT:
+priyanga
+a
+
+OUTPUT:
+2
+*/
